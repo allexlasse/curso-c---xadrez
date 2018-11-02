cd18528699150d4a2d1cf47a8424ec37ab35ec97
Allex Lasse <allex_lasse@hotmail.com>
2018-11-02 19:11:22 -0300

    Criadas classes que pertencem ao tabuleiro, inicio da criacao das pecas, impressao do tabuleiro e tratamento de erros personalizados

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -10,6 +8,21 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            try
+            { 
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 2));
+            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
+
+            Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,11 @@
+﻿
+namespace tabuleiro
+{
+    enum Cor
+    {
+        Branco,
+        Preto,
+        Vermelho,
+        Azul
+    }
+}
